c700062d163b231c40e29be55e546ea84e4496e3
Irfan Ahamed <irfanamd14@gmail.com>
2025-03-19 14:57:37 +0530

    major fixes

--- src/App.jsx
+++ src/App.jsx
@@ -67,7 +67,7 @@ const App = () => {
         (error) => {
           setLoading(false);
           toast.error('Turn your Location On');
-          setError("Turn your Location before trying");
+          setError("Turn your on Location before trying");
         },
         {
           enableHighAccuracy: true,
@@ -95,80 +95,84 @@ const App = () => {
   }  
 
   return (
-    <div className='w-[300px] h-[460px] bg-gradient-to-r from-[#141e30] to-[#243b55] rounded-md absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]'>
-      {
-        selectBtn &&
-        <>
-          <div className='absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] flex justify-center gap-4 w-72 text-white border py-2 rounded-md'>
-            <button onClick={ () => { setSearchByName(true), setSelectBtn(false) } } className='border-r pr-2'>Search by Name</button>
-            <button onClick={ () => { setSearchByLocation(true), setSelectBtn(false) } } >Search by Location</button>
-          </div>
-          <p className='text-white absolute bottom-1 left-2 animate-pulse'>By, Irfan Ahamed S</p>
-        </>
-      }
-      {
-        searchByName &&
-        <>
-          <div className='flex w-[280px] border rounded-full bg-gradient-to-r from-[#141e30] to-[#243b55] px-4 py-2 mt-6 mx-auto'>
-            <input className='w-full outline-none px-2 bg-transparent placeholder:text-gray-600 text-white' type="text" value={input} onChange={(e) => setInput(e.target.value)} placeholder="Enter city name" />
-            <button onClick={handleChange}><FaSearch className='text-white' /></button>
-          </div>
-          <FaArrowCircleLeft onClick={ clear } className='text-xl text-gray-300 mt-4 ml-3 cursor-pointer' />
-        </>
-      }
-      {
-        searchByLocation &&
-        <>
-          <div className='mt-6 text-center'>
-            <button onClick={ getLocation } className='text-gray-300 border rounded-full px-4 py-2 text-center'>Get your weather by Location<MdOutlineMyLocation className='inline ml-2' /></button>
-          </div>
-          <FaArrowCircleLeft onClick={ clear } className='text-xl text-gray-300 mt-4 ml-3 cursor-pointer' />
-        </>
-      }
-      {
-        loading ? 
-          <div className='absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]'>
-            <TbLoader3 className='animate-spin text-4xl text-slate-300' />
-          </div>
-        :
-        <div>
-          {response ? (
-            <div className='text-center'>
-                <img className='w-32 mx-auto relative' src={ `https://openweathermap.org/img/wn/${ response.weather[0].icon }@2x.png` } alt="" />
-                <p className=' text-xl font-bold text-gray-400'>{response.weather[0].description.toUpperCase()}</p>
-                <p className='text-4xl font-bold mt-3 text-white'>{Math.floor(response.main.temp)}°C</p>
-                <p className='text-gray-500 mt-2'>Feels Like {Math.floor(response.main.feels_like)}°C</p>
-                <h1 className='text-xl mt-2 font-[500] text-gray-300'>{response.name}</h1>
-                <div className='flex justify-around mt-4'>
-                  <div className='flex items-center gap-2'>
-                    <div>
-                      <WiHumidity className='text-gray-200 text-2xl' />
-                    </div>
-                    <div className='text-start'>
-                      <p className='text-white'>{response.main.humidity}</p>
-                      <p className='text-gray-200 text-sm'>Humidity</p>
-                    </div>
-                  </div>
-                  <div className='flex items-center gap-2'>
-                    <div>
-                      <FaWind className='text-gray-200 text-2xl' />
-                    </div>
-                    <div className='text-start'>
-                      <p className='text-white'>{response.wind.speed}</p>
-                      <p className='text-gray-200 text-sm'>Wind Speed</p>
-                    </div>
-                  </div>
-                </div>
+    <div className='bg-gradient-to-r from-[#141e30] to-[#243b55] w-full h-[100vh]'>
+      <div className='w-[320px] h-[480px] bg-gradient-to-r from-[#232526] to-[#414345] shadow-xl rounded-md absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]'>
+        {
+          selectBtn &&
+          <div className='p-8'>
+            <h1 className='text-orange-300 text-2xl font-bold text-center pt-5 animate-pulse'>Weather App</h1>
+            <p className='text-slate-400 text-sm text-center pt-2'>Search by using your location name or can search by simply turning on your location</p>
+            <div className='absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] flex justify-center gap-4 w-72 text-white border py-2 rounded-md'>
+              <button onClick={ () => { setSearchByName(true), setSelectBtn(false) } } className='border-r pr-2'>Search by Name</button>
+              <button onClick={ () => { setSearchByLocation(true), setSelectBtn(false) } } >Search by Location</button>
             </div>
-          ) :
-          <div className='absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] w-full'>
-            {
-              error && <p className='text-slate-200 text-center'>{ error }</p>
-            }
+            <p className='text-white absolute bottom-2 left-3 animate-pulse text-sm'>By, Irfan Ahamed S</p>
           </div>
         }
-        </div>
-      }
+        {
+          searchByName &&
+          <>
+            <div className='flex w-[280px] border rounded-full bg-gradient-to-r from-[#232526] to-[#414345] px-4 py-2 mt-10 mx-auto'>
+              <input className='w-full outline-none px-2 bg-transparent placeholder:text-gray-500 text-white' type="text" value={input} onChange={(e) => setInput(e.target.value)} placeholder="Enter city name" />
+              <button onClick={handleChange}><FaSearch className='text-white' /></button>
+            </div>
+            <FaArrowCircleLeft onClick={ clear } className='text-xl text-gray-300 mt-4 ml-6 cursor-pointer' />
+          </>
+        }
+        {
+          searchByLocation &&
+          <>
+            <div className='mt-10 text-center'>
+              <button onClick={ getLocation } className='text-gray-300 border rounded-full px-4 py-2 text-center'>Get your weather by Location<MdOutlineMyLocation className='inline ml-2' /></button>
+            </div>
+            <FaArrowCircleLeft onClick={ clear } className='text-xl text-gray-300 mt-4 ml-6 cursor-pointer' />
+          </>
+        }
+        {
+          loading ? 
+            <div className='absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]'>
+              <TbLoader3 className='animate-spin text-4xl text-slate-300' />
+            </div>
+          :
+          <div>
+            {response ? (
+              <div className='text-center'>
+                  <img className='w-32 mx-auto relative' src={ `https://openweathermap.org/img/wn/${ response.weather[0].icon }@2x.png` } alt="" />
+                  <p className=' text-xl font-bold text-gray-400'>{response.weather[0].description.toUpperCase()}</p>
+                  <p className='text-4xl font-bold mt-3 text-white'>{Math.floor(response.main.temp)}°C</p>
+                  <p className='text-gray-400 mt-2 font-semibold'>Feels Like {Math.floor(response.main.feels_like)}°C</p>
+                  <h1 className='text-xl mt-2 font-[500] text-gray-300'>{response.name}</h1>
+                  <div className='flex justify-around mt-4'>
+                    <div className='flex items-center gap-2'>
+                      <div>
+                        <WiHumidity className='text-gray-200 text-2xl' />
+                      </div>
+                      <div className='text-start'>
+                        <p className='text-white'>{response.main.humidity}</p>
+                        <p className='text-gray-200 text-sm'>Humidity</p>
+                      </div>
+                    </div>
+                    <div className='flex items-center gap-2'>
+                      <div>
+                        <FaWind className='text-gray-200 text-2xl' />
+                      </div>
+                      <div className='text-start'>
+                        <p className='text-white'>{response.wind.speed}</p>
+                        <p className='text-gray-200 text-sm'>Wind Speed</p>
+                      </div>
+                    </div>
+                  </div>
+              </div>
+            ) :
+            <div className='absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] w-full'>
+              {
+                error && <p className='text-slate-200 text-center'>{ error }</p>
+              }
+            </div>
+          }
+          </div>
+        }
+      </div>
     </div>
   );
 };
